24fa548bbd927572e7101f7bd83c88906b5963e6
Ankhaa-byambaa <25LP5026@nest.edu.mn>
2025-08-01 04:56:56 +0000

    something

---
+++ 07.30-Array/Array-Method-Exercise/exercise7.js
@@ -0,0 +1,74 @@
+let data = [
+  {
+    name: "iPhone 13",
+    category: "Electronics",
+    price: 1,
+    stock: 10,
+    brand: "Apple",
+    rating: 4.8,
+    isDiscounted: true,
+    discountPercent: 10,
+    supplier: "MobiCom",
+    weight: 0.3,
+  },
+  {
+    name: "Samsung Galaxy S22",
+    category: "Electronics",
+    price: 9,
+    stock: 5,
+    brand: "Samsung",
+    rating: 4.5,
+    isDiscounted: false,
+    discountPercent: 0,
+    supplier: "Unitel",
+    weight: 0.28,
+  },
+  {
+    name: "Dell XPS 13",
+    category: "Computers",
+    price: 2,
+    stock: 3,
+    brand: "Dell",
+    rating: 4.7,
+    isDiscounted: true,
+    discountPercent: 15,
+    supplier: "PCMall",
+    weight: 1.2,
+  },
+  {
+    name: "Mouse Logitech M590",
+    category: "Accessories",
+    price: 8,
+    stock: 20,
+    brand: "Logitech",
+    rating: 4.3,
+    isDiscounted: true,
+    discountPercent: 5,
+    supplier: "PCMall",
+    weight: 0.1,
+  },
+  {
+    name: "MacBook Air M2",
+    category: "Computers",
+    price: 3000000,
+    stock: 0,
+    brand: "Apple",
+    rating: 4.9,
+    isDiscounted: false,
+    discountPercent: 0,
+    supplier: "MobiCom",
+    weight: 1.1,
+  },
+];
+
+// 7. Бүх бүтээгдэхүүний үнэд 10% нэмдэг функц бич.
+function increasePriceByTenPercent(products) {
+  let dicountPercent = 10;
+  let num = 0;
+  let filtered = products.filter((product) => {
+    num = product.price / dicountPercent;
+    return (product.price += num);
+  });
+  return filtered;
+}
+console.log("dasgal 7 = ", increasePriceByTenPercent(data));
