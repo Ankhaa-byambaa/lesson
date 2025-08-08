8af0b8b24416ed2a53fe5d6067617e3d486df574
25LP5026 25LP5026 <25LP5026@G301-CC0207.local>
2025-08-08 13:26:50 +0800

    color2

---
+++ All-Lessons/08.08/color2.js
@@ -0,0 +1,91 @@
+const body = document.querySelector("body");
+const box1 = document.createElement("div");
+const box2 = document.createElement("div");
+const mainDiv = document.createElement("div");
+const mainDiv2 = document.createElement("div");
+const box3 = document.createElement("div");
+const box4 = document.createElement("div");
+
+box4.className = "box4";
+box3.className = "box3";
+box1.className = "box1";
+box2.className = "box2";
+mainDiv.className = "mainDiv";
+mainDiv2.className = "mainDiv2";
+body.className = "body";
+
+box1.style.width = "50%";
+box2.style.width = "50%";
+box3.style.width = "50%";
+box4.style.width = "50%";
+
+box1.style.height = "50vh";
+box2.style.height = "50vh";
+box3.style.height = "50vh";
+box4.style.height = "50vh";
+
+//neg
+
+document.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    let box4width = parseInt(getComputedStyle(box4).width);
+    let box1width = parseInt(getComputedStyle(box1).width);
+    let box2width = parseInt(getComputedStyle(box2).width);
+    let box3width = parseInt(getComputedStyle(box3).width);
+
+    let box4heigth = parseInt(getComputedStyle(box4).width);
+    let box1heigth = parseInt(getComputedStyle(box1).width);
+    let box2heigth = parseInt(getComputedStyle(box2).width);
+    let box3heigth = parseInt(getComputedStyle(box3).width);
+
+    //box1 tomorhod width-heigth
+    //box2 width--  ; heigth ++
+    //box3 heigth--; width ++
+    //box4 width--; heigth --
+
+    box1.style.width = box1width + 10 + "px";
+    box1.style.height = box1heigth + 10 + "vh";
+
+    // box2.style.width = box2width - 10 + "%";
+    // box2.style.height = box2heigth + 10 + "vh";
+
+    box3.style.width = box3width + 10 + "px";
+    box3.style.height = box3heigth - 10 + "vh";
+
+    box3.style.width = box4width - 10 + "px";
+    box4.style.height = box4heigth - 10 + "vh";
+  }
+});
+document.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    let redwidth = parseInt(getComputedStyle(box2).width);
+    let greenwidth = parseInt(getComputedStyle(box1).width);
+    box2.style.width = redwidth - 10 + "%";
+    box1.style.width = greenwidth + 10 + "%";
+  }
+  console.log();
+});
+
+box2.addEventListener("click", function (event) {
+  let redwidth = parseInt(getComputedStyle(box2).width);
+  let greenwidth = parseInt(getComputedStyle(box1).width);
+  box2.style.width = redwidth + 10 + "%";
+  box1.style.width = greenwidth - 10 + "%";
+});
+box1.addEventListener("click", function (event) {
+  let redwidth = parseInt(getComputedStyle(box2).width);
+  let greenwidth = parseInt(getComputedStyle(box1).width);
+  box2.style.width = redwidth - 10 + "%";
+  box1.style.width = greenwidth + 10 + "%";
+});
+console.log(box1.style.width);
+console.log(box2.style.width);
+//1. css bichne
+//herwee nogoon tal deer darwal 10px eer zai ni tomorno
+
+mainDiv.appendChild(box1);
+mainDiv.appendChild(box2);
+mainDiv2.appendChild(box3);
+mainDiv2.appendChild(box4);
+body.appendChild(mainDiv);
+body.appendChild(mainDiv2);
